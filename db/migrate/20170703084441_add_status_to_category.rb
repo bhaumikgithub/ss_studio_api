fa621b67e119308d0ae298bd2247c7da07ff3eae
arps1989dev <arpita.sojitra@gmail.com>
2017-07-06 11:55:36 +0530

    remove indentation in migration file

--- db/migrate/20170703084441_add_status_to_category.rb
+++ db/migrate/20170703084441_add_status_to_category.rb
@@ -1,4 +1,4 @@
-	class AddStatusToCategory < ActiveRecord::Migration[5.0]
+class AddStatusToCategory < ActiveRecord::Migration[5.0]
   def change
     add_column :categories, :status, :integer, :default => 1
   end
