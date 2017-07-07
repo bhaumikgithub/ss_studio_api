db6f1f4283245ee6d5f535d16ee4e90002c170ce
bhaumik gadani <bhaumikwork@gmail.com>
2017-07-07 14:34:00 +0530

    change routes in comment and put current resource in finding watermark

--- app/controllers/watermarks_controller.rb
+++ app/controllers/watermarks_controller.rb
@@ -4,20 +4,20 @@ class WatermarksController < ApplicationController
   # Callabcks
   before_action :fetch_watermark, only: [ :update ]
   
-  # GET /users/:user_id/watermarks
+  # GET /watermarks
   def index
     @watermarks = current_resource_owner.watermarks
     render_success_response({ :watermarks => @watermarks })
   end
 
-  # POST /users/:user_id/watermarks
+  # POST /watermarks
   def create
     @watermark = current_resource_owner.watermarks.create(resource_params)
     current_resource_owner.watermarks.where.not(id: @watermark.id).update_all(status: 0)
     render_success_response({ :watermarks => @watermark }, 201)
   end
 
-  # PATCH  /users/:user_id/watermarks/:id
+  # PATCH  /watermarks/:id
   def update
     super
     if @resource.status == "active"
@@ -31,6 +31,6 @@ class WatermarksController < ApplicationController
   end
 
   def fetch_watermark
-    @watermark = Watermark.find_by(id: params[:id])
+    @watermark = current_resource_owner.watermarks.find(id: params[:id])
   end
 end
