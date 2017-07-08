4402ecba6bf61ef8f28a1a546540297f70813725
Divyang Hirpara <diyanghirpara@gmail.com>
2017-07-08 14:18:57 +0530

    add rack cors for allow cross browser request

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,12 @@ module SsStudioApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:put, :patch, :delete, :get, :post, :options]
+      end
+    end
     config.autoload_paths << "#{Rails.root}/lib"
   end
 end
