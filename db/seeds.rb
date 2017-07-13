f552b9923842706acf44d628abe31634613a04f0
kinjalt <kinjalthakkar.tps@gmail.com>
2017-07-13 10:45:06 +0530

    uncomment seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user = User.create(email: 'sagar1@gmail.com', password: 'hello123', password_confirmation: 'hello123', first_name: 'sagar', last_name: 'gadani')
-# contact_details = ContactDetail.create(address: '2nd Floor, Tulsi Complex, Nr Azad Society, Behind Sahajanand College, Ambavadi, Ahmedabad', email: 'johndoe@gmail.com', phone: '+910123456789', user_id: user.id)
+user = User.create(email: 'sagar1@gmail.com', password: 'hello123', password_confirmation: 'hello123', first_name: 'sagar', last_name: 'gadani')
+
+contact_details = ContactDetail.create(address: '2nd Floor, Tulsi Complex, Nr Azad Society, Behind Sahajanand College, Ambavadi, Ahmedabad', email: 'johndoe@gmail.com', phone: '+910123456789', user_id: user.id)
 
 about = About.create(title_text: "A young photographer taking lovely shots", description: "We are capture best moments which is impossible to recapture", facebook_link: "www.facebook.com", twitter_link: "www.twitter.com", instagram_link: "www.instagram.com")
 
