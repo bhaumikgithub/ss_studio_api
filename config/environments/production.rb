43b686e5bdbe43f0ffd88da89c12f68fa5f39b2c
Divyang Hirpara <diyanghirpara@gmail.com>
2017-07-14 08:09:40 +0530

    set host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
+  config.action_mailer.default_url_options = { host: 'http://104.251.216.241/ror/ss_studio_api' }
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
