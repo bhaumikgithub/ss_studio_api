6db4db376c0e8b2cc71a460862f3d2435b2c68cc
arps1989dev <arpita.sojitra@gmail.com>
2017-07-19 11:40:27 +0530

    set the email template for public and private albums

--- app/controllers/album_recipients_controller.rb
+++ app/controllers/album_recipients_controller.rb
@@ -9,9 +9,18 @@ class AlbumRecipientsController < ApplicationController
     params[:album_recipient][:email].each do |email|
       @contact = Contact.find_or_create_by!(email: email) # email find if not find from contact then its create first
       album_recipients << @album.album_recipients.create!(album_recipient_params.merge(contact_id: @contact.id).permit!)
-      AlbumRecipientMailer.share_album_to_recipient_mail(album_recipients).deliver
+
+      if @album.is_private == false
+        AlbumRecipientMailer.share_public_album_to_recipient_mail( @contact, album_recipients, @album).deliver
+        @album_recipient = @album.album_recipients.where("ID IN (?)", album_recipients)
+        @album_recipient.update(is_email_sent: true)
+      else
+        AlbumRecipientMailer.share_private_album_to_recipient_mail( @contact, album_recipients, @album).deliver
+        @album_recipient = @album.album_recipients.where("ID IN (?)", album_recipients)
+        @album_recipient.update(is_email_sent: true)
+      end
     end
-    json_response({success: true, message: "Album share successfully.", data: {album_recipients: album_recipients}}, 201)
+    json_response({success: true, message: "Album share successfully.", data: {album_recipients: @album_recipient}}, 201)
   end
 
   private
--- app/mailers/album_recipient_mailer.rb
+++ app/mailers/album_recipient_mailer.rb
@@ -1,11 +1,19 @@
 class AlbumRecipientMailer < ApplicationMailer
   default from: "from@example.com"
 
-  def share_album_to_recipient_mail(contact, album_recipient, album)
+  def share_public_album_to_recipient_mail(contact, album_recipient, album)
     @contact = contact
     @album_recipient = album_recipient
     @album = album
-    @album_passcode = @album.passcode
     mail(from: "info@techplussoftware.com", to: contact.email , subject: "Share Album")
   end
+  
+  def share_private_album_to_recipient_mail(contact, album_recipient, album)
+    @contact = contact
+    @album_recipient = album_recipient
+    @album = album
+    @album_passcode = album.passcode
+    mail(from: "info@techplussoftware.com", to: contact.email , subject: "Share Album")
+  end
+
 end
