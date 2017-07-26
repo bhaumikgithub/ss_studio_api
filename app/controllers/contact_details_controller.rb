2a14f95600352f6eceb1af0c17b83da76af33914
paro2911 <parulk.tps@gmail.com>
2017-07-26 17:15:16 +0530

    fix comments

--- app/controllers/contact_details_controller.rb
+++ app/controllers/contact_details_controller.rb
@@ -1,11 +1,13 @@
 class ContactDetailsController < ApplicationController
-  skip_before_action :doorkeeper_authorize!
-  before_action :fetch_contact_detail, only: [ :show, :update]
+  skip_before_action :doorkeeper_authorize!, only: [ :show ]
+  before_action :fetch_contact_detail, only: [ :show, :update ]
 
+  # GET /contact_details
   def show
     render_success_response({ :contact_detail => @contact_detail}, 200)
   end
 
+  #PUT /contact_details
   def update
     @contact_detail.update_attributes!(contact_detail_param)
     render_success_response({ :contact_detail => @contact_detail}, 201)
