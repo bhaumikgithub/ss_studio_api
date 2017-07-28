8ea47afe0c5cb3b39e42f874a1f6c628f83b9adf
paro2911 <parulk.tps@gmail.com>
2017-07-28 16:16:42 +0530

    watermark module rspec

--- spec/models/watermark_spec.rb
+++ spec/models/watermark_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Watermark, type: :model do
       expect(assc.macro).to eq :has_one
     end
   end
+
+  describe 'Watermark' do
+    let(:status) do {
+      inactive: 0,
+      active: 1
+    }
+    end
+    subject { described_class.new }
+
+    it 'has valid a status' do
+      status.each do |type, value|
+        subject.status = value
+        subject.save
+        expect(subject.status).to eql(type.to_s)
+      end
+    end
+  end
 end
