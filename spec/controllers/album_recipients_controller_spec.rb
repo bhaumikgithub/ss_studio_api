fbaa216c39315ac3007e43e4aec37ed47b6ad089
paro2911 <parulk.tps@gmail.com>
2017-07-28 14:59:11 +0530

    album_recipient module and videos module rspec

--- spec/controllers/album_recipients_controller_spec.rb
+++ spec/controllers/album_recipients_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe AlbumRecipientsController, type: :controller do
+RSpec.describe AlbumRecipientsController, type: :request do
+  before do
+    @user = FactoryGirl.create(:user)
+    @contact = FactoryGirl.create(:contact)
+    @album = FactoryGirl.create(:album, user: @user) 
+    @album_recipient = FactoryGirl.create(:album_recipient, album: @album, contact: @contact)
+    @header = { Authorization: "bearer " + token_generator(@user) }
+  end
+  # =======================album_recipients#create=======================
+  describe 'POST /album_recipients' do
+    describe 'unauthorized' do
+      it "should return unauthorized" do
+        post "/albums/#{@album.id}/album_recipients", params: { album_recipient: { custom_message: 'Testing', email: ["foo@gmail.com", "bar@gmail.com"] } }
+        assert_response :unauthorized
+      end
+    end
 
+    describe 'authorized' do
+      context 'Successful' do
+        it 'create album_recipients' do
+          post "/albums/#{@album.id}/album_recipients", params: { album_recipient: { custom_message: 'Testing', email: ["foo@gmail.com", "bar@gmail.com"] } }, headers: @header
+          expect(response.status).to eq 201
+        end
+      end
+    end
+  end
 end
--- spec/models/album_recipient_spec.rb
+++ spec/models/album_recipient_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AlbumRecipient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Associations" do
+    it "belongs to album" do
+      assc = described_class.reflect_on_association(:album)
+      expect(assc.macro).to eq :belongs_to
+    end
+
+    it "belongs to contact" do
+      assc = described_class.reflect_on_association(:contact)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
 end
