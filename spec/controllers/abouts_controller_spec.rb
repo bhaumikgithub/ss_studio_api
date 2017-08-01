0847aa0705968ac0a61f7d86fa16df60f79c2223
paro2911 <parulk.tps@gmail.com>
2017-08-01 16:05:05 +0530

    Rspec changes in about module

--- spec/controllers/abouts_controller_spec.rb
+++ spec/controllers/abouts_controller_spec.rb
@@ -4,23 +4,16 @@ RSpec.describe AboutsController, type: :request do
   before do
     @user = FactoryGirl.create(:user)
     @photo = FactoryGirl.create(:photo, image: File.new(Rails.root + 'public/watermark.png'), user_id: @user.id)
-    @about = FactoryGirl.create(:about, photo: @photo)
+    @about = FactoryGirl.create(:about)
     @header = { Authorization: "bearer " + token_generator(@user) }
   end
 
-  # =======================abouts#index=======================
+  # =======================abouts#show=======================
   describe 'GET /abouts' do
-    describe 'unauthorized' do
-      it "should return unauthorized" do
-        get '/abouts'
-        assert_response :unauthorized
-      end
-    end
-
     describe 'authorized' do
       context 'Successful' do
         it 'returns all abouts' do
-          get '/abouts', headers: @header
+          get '/abouts'
           expect(response.status).to eq 200
         end
       end
@@ -31,7 +24,7 @@ RSpec.describe AboutsController, type: :request do
   describe 'PUT /abouts/:id' do
     describe 'unauthorized' do
       it "should return unauthorized" do
-        put "/abouts/#{@about.id}", params: { about: { title: 'title present' } }
+        put "/abouts", params: { about: { title: 'title present' } }
         assert_response :unauthorized
       end
     end
@@ -39,9 +32,7 @@ RSpec.describe AboutsController, type: :request do
     describe 'authorized' do
       context 'Successful' do
         it 'responds to PUT' do
-          # puts "=============#{Rack::Test::UploadedFile.new('public/download.jpg')}=============="
-          put "/abouts/#{@about.id}", params: { about: { title: 'hello', photo_attributes: { image: Rack::Test::UploadedFile.new('public/download.jpg'), id: @about.photo.id } } }, headers: @header
-          # puts "===========#{response.body.inspect}=============="
+          put "/abouts", params: { about: { title_text: 'hello', description: 'description' } }, headers: @header
           expect(response.status).to eq 201
         end
       end
