a24162be220799568143c3af2dd7a9b89159b278
kinjalt <kinjalthakkar.tps@gmail.com>
2017-08-01 14:48:54 +0530

    change url and change in seed file

--- app/serializers/abouts/photo_attributes_serializer.rb
+++ app/serializers/abouts/photo_attributes_serializer.rb
@@ -1,3 +1,6 @@
 class Abouts::PhotoAttributesSerializer < ActiveModel::Serializer
   attributes :image
+  def image
+    URI.join(ActionController::Base.asset_host,object.image.url).to_s
+  end
 end
