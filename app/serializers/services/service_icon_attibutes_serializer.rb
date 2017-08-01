f76ec8b070599ce1a074c26d4767c7ac7fd3157b
arps1989dev <arpita.sojitra@gmail.com>
2017-08-01 16:12:28 +0530

    remove commented code

--- app/serializers/services/service_icon_attibutes_serializer.rb
+++ app/serializers/services/service_icon_attibutes_serializer.rb
@@ -3,6 +3,5 @@ class Services::ServiceIconAttibutesSerializer < ActiveModel::Serializer
 
   def icon_image
     CommonSerializer.full_image_url(object.icon_image)
-    # URI.join(ActionController::Base.asset_host,object.icon_image).to_s 
   end
 end
