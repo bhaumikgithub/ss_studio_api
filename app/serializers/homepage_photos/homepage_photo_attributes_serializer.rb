379b493324b5934d18f184c99d50f935692abfee
arps1989dev <arpita.sojitra@gmail.com>
2017-08-01 16:09:31 +0530

    homepage galley image response change

--- app/serializers/homepage_photos/homepage_photo_attributes_serializer.rb
+++ app/serializers/homepage_photos/homepage_photo_attributes_serializer.rb
@@ -1,9 +1,11 @@
 class HomepagePhotos::HomepagePhotoAttributesSerializer < ActiveModel::Serializer
   attributes :id, :is_active, :homepage_image, :homepage_image_file_name
-  belongs_to :photo, key: "photo", serializer: HomepagePhotos::ImageAttibutesSerializer
 
   def homepage_image
-    if object.homepage_image.present?
-    end
+      CommonSerializer.full_image_url(object.homepage_image.present? ? object.homepage_image.url : object.photo.image.url)
+  end
+
+  def homepage_image_file_name
+    object.homepage_image_file_name.present? ? object.homepage_image_file_name : object.photo.image_file_name
   end
 end
--- app/serializers/services/service_icon_attibutes_serializer.rb
+++ app/serializers/services/service_icon_attibutes_serializer.rb
@@ -2,6 +2,7 @@ class Services::ServiceIconAttibutesSerializer < ActiveModel::Serializer
   attributes :icon_image
 
   def icon_image
-    URI.join(ActionController::Base.asset_host,object.icon_image).to_s 
+    CommonSerializer.full_image_url(object.icon_image)
+    # URI.join(ActionController::Base.asset_host,object.icon_image).to_s 
   end
 end
