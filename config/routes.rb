6dd0bd501948abff1323046d614ceb6d4dd1ef5d
arps1989dev <arpita.sojitra@gmail.com>
2017-08-18 15:08:19 +0530

    create service icon controller for get service icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   
-  # get 'service_icon/index'
 
   get 'users/show'
 
--- spec/controllers/service_icons_controller_spec.rb
+++ spec/controllers/service_icons_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ServiceIconController, type: :controller do
+RSpec.describe ServiceIconsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
