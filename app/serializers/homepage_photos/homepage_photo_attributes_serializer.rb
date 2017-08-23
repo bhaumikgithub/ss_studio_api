f3dc7fab286df1b7399df0c5f636797385065377
arps1989dev <arpita.sojitra@gmail.com>
2017-08-23 18:49:46 +0530

    get active photos in home page gallery

--- app/serializers/homepage_photos/homepage_photo_attributes_serializer.rb
+++ app/serializers/homepage_photos/homepage_photo_attributes_serializer.rb
@@ -2,7 +2,7 @@ class HomepagePhotos::HomepagePhotoAttributesSerializer < ActiveModel::Serialize
   attributes :id, :is_active, :homepage_image, :homepage_image_file_name
 
   def homepage_image
-      CommonSerializer.full_image_url(object.homepage_image.present? ? object.homepage_image.url : object.photo.image.url)
+      CommonSerializer.full_image_url(object.homepage_image.present? ? object.homepage_image.url(:medium) : object.photo.image.url(:medium))
   end
 
   def homepage_image_file_name
