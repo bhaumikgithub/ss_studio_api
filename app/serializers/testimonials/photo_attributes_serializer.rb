8a3df75d21b9e3138d12c7bdf1035e29911472a5
Tps <you@example.com>
2017-08-26 12:43:12 +0530

    Add style variation in testimonial API and remove unused styles from paperclip

--- app/serializers/testimonials/photo_attributes_serializer.rb
+++ app/serializers/testimonials/photo_attributes_serializer.rb
@@ -1,6 +1,7 @@
 class Testimonials::PhotoAttributesSerializer < ActiveModel::Serializer
   attributes :image_file_name, :image
   def image
-    CommonSerializer.full_image_url(object.image.url)
+  	style = instance_options[:style].present? ? instance_options[:style] : "original"
+    CommonSerializer.full_image_url(object.image.url(style))
   end
 end
