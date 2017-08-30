599bd665eebe18c8220dba339155f4f3defd5646
kinjalt <kinjalthakkar.tps@gmail.com>
2017-08-30 18:11:22 +0530

    put validation for category and change in update_attributes

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -46,7 +46,7 @@ class AlbumsController < ApplicationController
   
   # PATCH/PUT /albums/:id
   def update
-    @album.update_attributes(album_params)
+    @album.update_attributes!(album_params)
     # json_response({success: true, message: "Album update successfully.", data: {album: @album}}, 201)
     json_response({
       success: true,
