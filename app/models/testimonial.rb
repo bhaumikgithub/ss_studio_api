5ea873045f01bcaca8948a7fca7583baa2dc2f73
Ashvin chauhan <ashvin.tps@gmail.com>
2017-08-30 16:04:53 +0530

    Add validation in testimonial page

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,10 +1,19 @@
 class Testimonial < ApplicationRecord
   acts_as_paranoid
 
+  # Callabcks
+
+  # Associations
+
   # Associations
   belongs_to :contact
   belongs_to :user
   has_one :photo, as: :imageable, dependent: :destroy
 
-  accepts_nested_attributes_for :photo
+  accepts_nested_attributes_for :photo, reject_if: proc { |attributes| attributes['image'].blank? }
+  # Enumerator
+
+  # Validations
+  validates :client_name, :message, :photo, presence: true
+  validates :rating, :numericality => {:greater_than => 0}
 end
