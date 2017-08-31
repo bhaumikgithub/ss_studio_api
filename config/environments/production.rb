fd09026066013c5962c1d07a41600089cf775761
Ashvin chauhan <ashvin.tps@gmail.com>
2017-08-31 19:24:38 +0530

    Change mail sender from smtp to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,16 +70,25 @@ Rails.application.configure do
   # config.logger = Logger.new(STDOUT)
 
 
-  config.action_mailer.default_url_options = { host: 'http://104.251.216.241/ror/ss_studio_api' }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: 'http://104.251.216.241/ror/ss_studio_api' }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'gmail.com',
+  #   :user_name            => ENV['GMAIL_USER_NAME'],
+  #   :password             => ENV['GMAIL_PASSWORD'],
+  #   :authentication       => 'plain'
+  # }
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "smtp.sendgrid.net",
     :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => ENV['GMAIL_USER_NAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => 'plain'
+    :domain               => 'sagargadani.com',
+    :user_name            => ENV['SENDGRID_USER_NAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => :plain,
+    :enable_starttls_auto => true
   }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
