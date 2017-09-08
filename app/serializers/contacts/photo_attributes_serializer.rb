e08c52daa428664a79fd37afdc7af6fe4c2b4050
paro2911 <parulk.tps@gmail.com>
2017-09-08 17:22:35 +0530

    changes in contact serializer

--- app/serializers/contacts/photo_attributes_serializer.rb
+++ app/serializers/contacts/photo_attributes_serializer.rb
@@ -2,6 +2,7 @@ class Contacts::PhotoAttributesSerializer < ActiveModel::Serializer
   attributes :image_file_name, :image
 
   def image
-    CommonSerializer.full_image_url(object.image.url)
+    style = instance_options[:style].present? ? instance_options[:style] : "original"
+    CommonSerializer.full_image_url(object.image.url(style))
   end
 end
