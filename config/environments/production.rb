acad7c725e830dcf2e1228914baee9d4d1feda11
paro2911 <parulk.tps@gmail.com>
2017-11-03 17:32:12 +0530

    changes in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,12 @@ Rails.application.configure do
   # }
 
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.elasticemail.com",
-    :port                 => 2525,
-    :user_name            => ENV['ELASTICMAIL_USER_NAME'],
-    :password             => ENV['ELASTICMAIL_PASSWORD'],
-    :authentication       => 'login',
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
     :domain               => 'sagargadani.com',
+    :user_name            => ENV['SENDGRID_USER_NAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
 
