23441e114b7fd26b79c31972f0c8b606d6124c6c
paro2911 <parulk.tps@gmail.com>
2017-11-16 11:50:27 +0530

    add comment in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-frozen_string_literal: true
-This file should contain all the record creation needed to seed the database with its default values.
-The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# frozen_string_literal: true
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Examples:
+# Examples:
 
-  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create(email: 'sagar1@gmail.com', password: 'hello123', password_confirmation: 'hello123', first_name: 'sagar', last_name: 'gadani')
 
