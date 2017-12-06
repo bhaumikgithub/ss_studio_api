0820ab9dc759806a50e90588be1a937e49c300af
Ashvin chauhan <ashvin.tps@gmail.com>
2017-12-06 16:45:26 +0530

    Change indentation space from 4 to 2

--- app/services/import_google_contacts.rb
+++ app/services/import_google_contacts.rb
@@ -1,23 +1,23 @@
 class ImportGoogleContacts < BaseService
-	attr_accessor :access_token, :current_user
+  attr_accessor :access_token, :current_user
 
-	def initialize(access_token, current_user)
-		@access_token = access_token
-		@current_user = current_user
-	end
+  def initialize(access_token, current_user)
+    @access_token = access_token
+    @current_user = current_user
+  end
 
-	def call
-		import_contacts
-	end
+  def call
+    import_contacts
+  end
 
-	private
+  private
 
-	def get_contacts
-		JSON.parse(open("https://www.google.com/m8/feeds/contacts/default/full?max-results=50&alt=json", "Authorization" =>  "Bearer #{access_token}", "GData-Version" => "3.0").read)
-	end
+  def get_contacts
+    JSON.parse(open("https://www.google.com/m8/feeds/contacts/default/full?max-results=50&alt=json", "Authorization" =>  "Bearer #{access_token}", "GData-Version" => "3.0").read)
+  end
 
-	def contacts
-		get_contacts["feed"]["entry"].collect{ |p|
+  def contacts
+    get_contacts["feed"]["entry"].collect{ |p|
       {
         first_name: (p["gd$name"]["gd$givenName"]["$t"] unless p["gd$name"].nil?),
         last_name:  (p["gd$name"]["gd$familyName"]["$t"] unless p["gd$name"].nil? || p["gd$name"]["gd$familyName"].nil?),
@@ -25,15 +25,15 @@ class ImportGoogleContacts < BaseService
         phone: (p["gd$phoneNumber"][0]["$t"] unless p["gd$phoneNumber"].nil?)
       }
     }
-	end
+  end
 
-	def import_contacts
-		contacts.each do |contact|
+  def import_contacts
+    contacts.each do |contact|
       begin
-      	Contact.create_contact(contact[:email], current_user)
+        Contact.create_contact(contact[:email], current_user)
       rescue StandardError => e
         puts "===#{e.inspect}======"
       end
     end
-	end
+  end
 end
