cdc58f33efc6aa53ffd69c605ab8a64181b9a10d
parulkanani <bhaumikwork@gmail.com>
2018-08-21 10:47:02 +0530

    confirmation url issue fixed

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,7 +6,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     yield resource if block_given?
     if resource.errors.empty?
       set_flash_message!(:notice, :confirmed)
-      redirect_to "http://localhost:3000/admin"
+      redirect_to  ENV['FRONT_URL']
     else
       respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
