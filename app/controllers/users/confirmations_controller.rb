5e38ef3812bcbd5c1a52c0829517da9811967b02
kinjalt <kinjalthakkar.tps@gmail.com>
2018-08-21 11:25:24 +0530

    set user confirm after status updated to active

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,7 +6,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     yield resource if block_given?
     if resource.errors.empty?
       set_flash_message!(:notice, :confirmed)
-      redirect_to  ENV['FRONT_URL']
+      redirect_to  ENV['FRONT_URL']+'admin'
     else
       respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
