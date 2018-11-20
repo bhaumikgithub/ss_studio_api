4a7ff0b1a7e4720c3831e8837cd9ac9b0a0dc1ec
kinjalt <kinjalthakkar.tps@gmail.com>
2018-11-20 16:30:29 +0530

    hot fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
 
   # GET   /categories
   def index
-    @categories = current_resource_owner.categories.active
+    @categories = current_resource_owner.categories
     render_success_response({ :categories => @categories },200)
   end
 
