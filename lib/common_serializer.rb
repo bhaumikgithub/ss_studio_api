92dc5f9acaa68854d59e845d4efc2d2df17ae3f7
kinjalt <kinjalthakkar.tps@gmail.com>
2019-01-23 15:39:22 +0530

    change assets

--- lib/common_serializer.rb
+++ lib/common_serializer.rb
@@ -1,6 +1,6 @@
 class CommonSerializer
   def self.full_image_url(image_url)
-    (ActionController::Base.asset_host + image_url).to_s
+    (ActionController::Base.asset_host + '/sites' + image_url).to_s
   end
 
   def self.date_formate(date)
