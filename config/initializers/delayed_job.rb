a8a92f4b6995b367dc0eeb90e492db80e5306bb7
Divyang Hirpara <divyanghirpara@gmail.com>
2019-01-25 12:16:48 +0000

    changes in delayed job and font awsome

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,6 @@
-Delayed::Worker.logger = Logger.new("log/delayed_job.log", 5, 104857600)
+log_file = File.open("log/delayed_job.log", 5, 104857600)
+Delayed::Worker.logger = Logger.new(log_file)
+#Delayed::Worker.logger = Logger.new("log/delayed_job.log", 5, 104857600)
 
 if caller.last =~ /bin\/delayed_job/ or (File.basename($0) == "rake" and ARGV[0] =~ /jobs\:work/)
   ActiveRecord::Base.logger = Delayed::Worker.logger
