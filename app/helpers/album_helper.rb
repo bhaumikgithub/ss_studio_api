0be2aceba04488e16227a5a8d9ae2b5c8eaba40a
kinjalt <kinjalthakkar.tps@gmail.com>
2019-01-29 17:15:59 +0530

    change in portfolio detail page and resolved issue of menu in mobile view

--- app/helpers/album_helper.rb
+++ app/helpers/album_helper.rb
@@ -2,4 +2,11 @@ module AlbumHelper
    def active_class(link_path)
     current_page?(link_path) ? "active" : ""
   end
+  def display_photo_count(album, photos)
+    if params[:page].nil? && photos.count > 0
+      "Showing 1 to " + photos.count.to_s + " from " + album.photos.count.to_s + " photos"
+    else
+      "Showing " + ( ( ( params[:page].to_i - 1 ) * 32 ) + 1 ).to_s + " to "  + ( ( 32 * (params[:page].to_i - 1)) + photos.count ).to_s + " from " + album.photos.count.to_s + " photos"
+    end
+  end
 end
