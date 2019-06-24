24245c9382ae84fde11b06ec87b56ab220035755
Bhaumik <bhaumikgithub@gmail.com>
2019-06-24 10:57:49 +0530

    added favicon in aws

--- app/models/website_detail.rb
+++ app/models/website_detail.rb
@@ -2,21 +2,16 @@ class WebsiteDetail < ApplicationRecord
   belongs_to :user
 
   has_attached_file :favicon_image,
-                    :styles => {
-                      :original => {
-                        :geometry => "1920x1920>"
+                    :style => { :original => '300x400', :thumb => '120x120' },
+                    storage: :s3,
+                    s3_region: ENV['AWS_S3_REGION'],
+                    s3_credentials: {
+                      s3_host_name: ENV['AWS_S3_HOST_NAME'],
+                      bucket: ENV['AWS_S3_BUCKET_PROD'],
+                      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
                       },
-                      :large => {
-                        :geometry => "1024x768#"
-                      },
-                      :medium => {
-                        :geometry => "270x270#"
-                      },
-                      :thumb => {
-                        :geometry => "500x1000#"
-                      }
-                    },
-                    default_url: ""
+                    :s3_protocol => :https
   validates_attachment_content_type :favicon_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/svg", "image/vnd.microsoft.icon", "image/x-icon"]
 
   validates :title, :copyright_text, presence: true
