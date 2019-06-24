173ddba3cdeccff68d81899ff49ccb642591359e
Bhaumik <bhaumikgithub@gmail.com>
2019-06-24 12:07:17 +0530

    added dynamic year

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,7 +1,7 @@
-log_file = File.open("log/delayed_job.log", 5, 104857600)
-Delayed::Worker.logger = Logger.new(log_file)
-#Delayed::Worker.logger = Logger.new("log/delayed_job.log", 5, 104857600)
+# log_file = File.open("log/delayed_job.log", 5, 104857600)
+# Delayed::Worker.logger = Logger.new(log_file)
+# #Delayed::Worker.logger = Logger.new("log/delayed_job.log", 5, 104857600)
 
-if caller.last =~ /bin\/delayed_job/ or (File.basename($0) == "rake" and ARGV[0] =~ /jobs\:work/)
-  ActiveRecord::Base.logger = Delayed::Worker.logger
-end
+# if caller.last =~ /bin\/delayed_job/ or (File.basename($0) == "rake" and ARGV[0] =~ /jobs\:work/)
+#   ActiveRecord::Base.logger = Delayed::Worker.logger
+# end
