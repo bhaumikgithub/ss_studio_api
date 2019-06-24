5db41ef21179b5e2928a2a84064376554f18661c
Bhaumik <bhaumikgithub@gmail.com>
2019-06-24 11:08:54 +0530

    favicon store in local if ENV is development

--- app/models/website_detail.rb
+++ app/models/website_detail.rb
@@ -1,17 +1,23 @@
 class WebsiteDetail < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :favicon_image,
-                    :style => { :original => '300x400', :thumb => '120x120' },
-                    storage: :s3,
-                    s3_region: ENV['AWS_S3_REGION'],
-                    s3_credentials: {
-                      s3_host_name: ENV['AWS_S3_HOST_NAME'],
-                      bucket: ENV['AWS_S3_BUCKET_PROD'],
-                      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-                      },
-                    :s3_protocol => :https
+  paperclip_opts = {
+    :style => { :original => '300x400', :thumb => '120x120' },
+  }
+
+  unless Rails.env.development?
+    paperclip_opts.merge! storage: :s3,
+                          s3_region: ENV['AWS_S3_REGION'],
+                          s3_credentials: {
+                            s3_host_name: ENV['AWS_S3_HOST_NAME'],
+                            bucket: ENV['AWS_S3_BUCKET_PROD'],
+                            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+                          },
+                        :s3_protocol => :https
+  end
+  has_attached_file :favicon_image, paperclip_opts
+
   validates_attachment_content_type :favicon_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/svg", "image/vnd.microsoft.icon", "image/x-icon"]
 
   validates :title, :copyright_text, presence: true
--- app/serializers/website_details/website_detail_attributes_serializer.rb
+++ app/serializers/website_details/website_detail_attributes_serializer.rb
@@ -1,6 +1,10 @@
 class WebsiteDetails::WebsiteDetailAttributesSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :favicon_image
   def favicon_image
-    CommonSerializer.full_image_url(object&.favicon_image&.url(:original)) if object.favicon_image.present?
+    if Rails.env.development?
+      CommonSerializer.full_image_url(ActionController::Base.asset_host + object&.favicon_image&.url(:original)) if object.favicon_image.present?
+    else
+      CommonSerializer.full_image_url(object&.favicon_image&.url(:original)) if object.favicon_image.present?
+    end
   end
 end
