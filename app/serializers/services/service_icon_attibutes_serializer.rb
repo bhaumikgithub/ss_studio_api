fe2163ed1edd92dfbb7590d2e879d3ebe3b0d385
parulkanani <parulk.tps@gmail.com>
2019-06-27 12:20:26 +0530

    change url path

--- app/serializers/services/service_icon_attibutes_serializer.rb
+++ app/serializers/services/service_icon_attibutes_serializer.rb
@@ -2,6 +2,6 @@ class Services::ServiceIconAttibutesSerializer < ActiveModel::Serializer
   attributes :icon_image
 
   def icon_image
-    CommonSerializer.full_image_url(object.icon_image)
+    CommonSerializer.watermark_full_image_url(object.icon_image)
   end
 end
